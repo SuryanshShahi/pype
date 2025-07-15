025deacb989e790f07051f7088d4f9db40ce9a46
S-aeriax <os001@aerial.world>
2025-07-15 10:26:09 +0530

    FEAT: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from "next/navigation";
-import HomePage from "./(pages)/home/page";
-import PageWrapper from "./components/pageWrapper/PageWrapper";
 
 export default function Home() {
   redirect("/home");
-  return;
 }
